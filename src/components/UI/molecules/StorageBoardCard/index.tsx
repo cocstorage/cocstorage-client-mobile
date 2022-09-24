7f4ac897d1dcf8374036e965362e85c9664c7d53
kimjh96 <kimjhs@kakao.com>
2022-09-24 12:48:22 +0900

    fix: 방문 표시가 되지 않던 부분 수정

--- src/components/UI/molecules/StorageBoardCard/index.tsx
+++ src/components/UI/molecules/StorageBoardCard/index.tsx
@@ -65,9 +65,9 @@ function StorageBoardCard({
 
   if (variant === 'normal') {
     return (
-      <Link href={`/storages/${path}/${id}`}>
-        <a>
-          <StyledStorageBoardCard>
+      <StyledStorageBoardCard>
+        <Link href={`/storages/${path}/${id}`}>
+          <a>
             <Wrapper variant={variant} hasThumbnail={!!thumbnailUrl} {...props} css={customStyle}>
               <Box customStyle={{ borderRadius: round, overflow: 'hidden', zIndex: 1 }}>
                 <Image
@@ -157,16 +157,16 @@ function StorageBoardCard({
                 </Info>
               </Flexbox>
             </Wrapper>
-          </StyledStorageBoardCard>
-        </a>
-      </Link>
+          </a>
+        </Link>
+      </StyledStorageBoardCard>
     );
   }
 
   return (
-    <Link href={`/storages/${path}/${id}`}>
-      <a>
-        <StyledStorageBoardCard>
+    <StyledStorageBoardCard>
+      <Link href={`/storages/${path}/${id}`}>
+        <a>
           <Wrapper variant={variant} hasThumbnail={!!thumbnailUrl} {...props} css={customStyle}>
             <Flexbox
               direction="vertical"
@@ -278,9 +278,9 @@ function StorageBoardCard({
               </Box>
             )}
           </Wrapper>
-        </StyledStorageBoardCard>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </StyledStorageBoardCard>
   );
 }
 
