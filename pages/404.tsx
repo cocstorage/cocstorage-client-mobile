f0f7ace9872173485247bd6ba8005cd9ed9defe5
kimjh96 <kimjhs@kakao.com>
2022-09-29 22:16:09 +0900

    feat: 404, 500 페이지 theme-color 지정

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Button, Flexbox } from 'cocstorage-ui';
+import { Button, Flexbox, useTheme } from 'cocstorage-ui';
 
 import GeneralTemplate from '@components/templeates/GeneralTemplate';
 import Message from '@components/UI/molecules/Message';
@@ -8,28 +9,39 @@ import Message from '@components/UI/molecules/Message';
 function Error404() {
   const router = useRouter();
 
+  const {
+    theme: {
+      palette: { background }
+    }
+  } = useTheme();
+
   const handleClick = () => router.push('/');
 
   return (
-    <GeneralTemplate disableFlexible={false}>
-      <Flexbox direction="vertical" customStyle={{ height: '100%', padding: '20px 0' }}>
-        <Flexbox direction="vertical" justifyContent="center" customStyle={{ flexGrow: 1 }}>
-          <Message
-            title="존재하지 않는 페이지네요."
-            message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
-            hideButton
-          />
+    <>
+      <Head>
+        <meta name="theme-color" content={background.bg} />
+      </Head>
+      <GeneralTemplate disableFlexible={false}>
+        <Flexbox direction="vertical" customStyle={{ height: '100%', padding: '20px 0' }}>
+          <Flexbox direction="vertical" justifyContent="center" customStyle={{ flexGrow: 1 }}>
+            <Message
+              title="존재하지 않는 페이지네요."
+              message="찾으시는 항목이 삭제되었거나,<br /> 잘못된 주소가 입력되었을 수 있어요."
+              hideButton
+            />
+          </Flexbox>
+          <Button
+            fullWidth
+            size="big"
+            onClick={handleClick}
+            customStyle={{ justifyContent: 'center' }}
+          >
+            홈으로
+          </Button>
         </Flexbox>
-        <Button
-          fullWidth
-          size="big"
-          onClick={handleClick}
-          customStyle={{ justifyContent: 'center' }}
-        >
-          홈으로
-        </Button>
-      </Flexbox>
-    </GeneralTemplate>
+      </GeneralTemplate>
+    </>
   );
 }
 
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,6 +1,7 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { Button, Flexbox } from 'cocstorage-ui';
+import { Button, Flexbox, useTheme } from 'cocstorage-ui';
 
 import GeneralTemplate from '@components/templeates/GeneralTemplate';
 import Message from '@components/UI/molecules/Message';
@@ -8,28 +9,39 @@ import Message from '@components/UI/molecules/Message';
 function Error500() {
   const router = useRouter();
 
+  const {
+    theme: {
+      palette: { background }
+    }
+  } = useTheme();
+
   const handleClick = () => router.push('/');
 
   return (
-    <GeneralTemplate disableFlexible={false}>
-      <Flexbox direction="vertical" customStyle={{ height: '100%', padding: '20px 0' }}>
-        <Flexbox direction="vertical" justifyContent="center" customStyle={{ flexGrow: 1 }}>
-          <Message
-            title="알 수 없는 오류가 발생했어요."
-            message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
-            hideButton
-          />
+    <>
+      <Head>
+        <meta name="theme-color" content={background.bg} />
+      </Head>
+      <GeneralTemplate disableFlexible={false}>
+        <Flexbox direction="vertical" customStyle={{ height: '100%', padding: '20px 0' }}>
+          <Flexbox direction="vertical" justifyContent="center" customStyle={{ flexGrow: 1 }}>
+            <Message
+              title="알 수 없는 오류가 발생했어요."
+              message="요청을 처리하지 못했어요.<br />잠시 후에 다시 시도해 주세요!"
+              hideButton
+            />
+          </Flexbox>
+          <Button
+            fullWidth
+            size="big"
+            onClick={handleClick}
+            customStyle={{ justifyContent: 'center' }}
+          >
+            홈으로
+          </Button>
         </Flexbox>
-        <Button
-          fullWidth
-          size="big"
-          onClick={handleClick}
-          customStyle={{ justifyContent: 'center' }}
-        >
-          홈으로
-        </Button>
-      </Flexbox>
-    </GeneralTemplate>
+      </GeneralTemplate>
+    </>
   );
 }
 
